fa27dfd11d05ac4cac05a1c3842b5c6f15f11915
Duelsol <duelsol@sina.com>
2018-06-07 21:18:03 +0800

    no message

---
+++ src/main/java/me/duelsol/springmvcseed/framework/support/ResponseData.java
@@ -0,0 +1,48 @@
+package me.duelsol.springmvcseed.framework.support;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: 冯奕骅
+ * Date: 2018/6/6
+ * Time: 19:47
+ */
+public class ResponseData {
+    /**
+     * 编号，非"0"时都表示发生错误。
+     */
+    private String code = null;
+    /**
+     * 消息，在错误时说明具体原因。
+     */
+    private String message = null;
+    /**
+     * 数据。
+     */
+    private Object data = null;
+
+    public ResponseData() {
+        this.code = "0";
+    }
+
+    public ResponseData(Object data) {
+        this.code = "0";
+        this.data = data;
+    }
+
+    public ResponseData(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}

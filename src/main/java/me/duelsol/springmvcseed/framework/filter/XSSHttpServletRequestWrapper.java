61c417c5682fbcc0a7372b6fdbc1f9382b6a1971
Duelsol <duelsol@sina.com>
2019-03-01 22:11:27 +0800

    no message

--- src/main/java/me/duelsol/springmvcseed/framework/filter/XSSHttpServletRequestWrapper.java
+++ src/main/java/me/duelsol/springmvcseed/framework/filter/XSSHttpServletRequestWrapper.java
@@ -1,6 +1,6 @@
 package me.duelsol.springmvcseed.framework.filter;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;

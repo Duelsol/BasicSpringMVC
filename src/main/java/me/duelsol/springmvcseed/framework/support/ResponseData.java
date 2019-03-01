37320f2ad6b7a47fa1213f0698d5be2e08c6a2ed
Duelsol <duelsol@sina.com>
2019-03-02 01:08:42 +0800

    no message

--- src/main/java/me/duelsol/springmvcseed/framework/support/ResponseData.java
+++ src/main/java/me/duelsol/springmvcseed/framework/support/ResponseData.java
@@ -1,20 +1,25 @@
 package me.duelsol.springmvcseed.framework.support;
 
+import java.io.Serializable;
+
 /**
  * Created by IntelliJ IDEA.
  * User: 冯奕骅
  * Date: 2018/6/6
  * Time: 19:47
  */
-public class ResponseData {
+public class ResponseData implements Serializable {
+
     /**
      * 编号，非"0"时都表示发生错误。
      */
     private String code = null;
+
     /**
      * 消息，在错误时说明具体原因。
      */
     private String message = null;
+
     /**
      * 数据。
      */
@@ -45,4 +50,5 @@ public class ResponseData {
     public Object getData() {
         return data;
     }
+
 }
